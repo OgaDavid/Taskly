0c2ee69030271826cac87d7ccfafddafb8322f3c
OgaDavid <oluwademiladedavidoga@gmail.com>
2024-03-16 22:20:40 +0100

    add inter font

--- src/components/site-header.tsx
+++ src/components/site-header.tsx
@@ -1,10 +1,20 @@
+import { cn } from "@/lib/utils";
 import { Icons } from "./icons";
+import { buttonVariants } from "./ui/button";
 
 const SiteHeader = () => {
   return (
     <header>
       <Icons.logo />
-      <div>
+      Hello
+      <div
+        className={cn(
+          buttonVariants({
+            variant: "ghost",
+          }),
+          "w-9 px-0"
+        )}
+      >
         <Icons.gitHub className="h-4 w-4" />
         <span className="sr-only">GitHub</span>
       </div>
