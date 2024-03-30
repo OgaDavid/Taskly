0d334064872a35b2faa448278401b537db920f4c
OgaDavid <oluwademiladedavidoga@gmail.com>
2024-03-30 11:47:23 +0100

    fix

--- src/components/tasks-container.tsx
+++ src/components/tasks-container.tsx
@@ -3,7 +3,7 @@ import TaskCard from "./task-card";
 
 const TasksContainer = ({ tasks }: { tasks: Task[] }) => {
   return (
-    <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-3 md:gap-5">
+    <div className="grid max-[475px]:grid-cols-1 grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-3 md:gap-5">
       {tasks.map((task) => (
         <TaskCard key={task.id} task={task} />
       ))}
