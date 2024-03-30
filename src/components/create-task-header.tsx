59ef4aca31f0596f4b94e54940945370671e9abd
OgaDavid <oluwademiladedavidoga@gmail.com>
2024-03-30 11:02:20 +0100

    style create task header🚀

--- src/components/create-task-header.tsx
+++ src/components/create-task-header.tsx
@@ -2,6 +2,7 @@ import { getToday } from "@/lib/utils";
 import { Button } from "@/components/ui/button";
 import { CalendarDays, Plus } from "lucide-react";
 import { useCreateTaskModalStore } from "@/hooks/use-create-task-modal";
+import useMediaQuery from "@/hooks/use-media-query";
 
 const CreateTaskHeader = () => {
   const onOpen = useCreateTaskModalStore((state) => state.onOpen);
@@ -13,18 +14,20 @@ const CreateTaskHeader = () => {
     }
   };
 
+  const isDesktop = useMediaQuery("(min-width: 768px)");
+
   return (
     <div className="flex items-center justify-between">
       <div>
-        <h1 className="text-3xl pb-1">My Tasks 🚀</h1>
-        <p className="text-custom-neutral/50 flex items-center inter-regular font-medium text-sm">
+        <h1 className="text-lg md:text-3xl pb-1">My Tasks 🚀</h1>
+        <p className="text-custom-neutral/50 flex items-center inter-regular font-medium text-xs md:text-sm">
           <CalendarDays className="w-4 h-4 mr-2" />
           {getToday()}
         </p>
       </div>
       <Button onClick={openCreateTaskModal}>
-        Create new task
-        <Plus className="w-4 h-4 ml-2" />
+        {isDesktop ? "Create New Task" : "Add Task"}
+        <Plus className="w-4 font h-4 ml-2" />
       </Button>
     </div>
   );
